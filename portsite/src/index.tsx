c0aae391a5b416321516df0ad7b6f6cc472a173f
Mitsune-e <gveloso1414@gmail.com>
2023-01-08 23:56:27 -0300

    Adding initial navbar and bootstrap

--- portsite/src/index.tsx
+++ portsite/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './components';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(
--- portsite/src/pages/Home/index.tsx
+++ portsite/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { Navbar } from '../../components/index';
 
 export const Home: React.FC = () => {
   return (
     <>
+      <Navbar />
       <div className="Home">
+
         <p>Batatinha</p>
       </div>
     </>
